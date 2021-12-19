b7e1bff05b8cdd75e566945a6f8b04297eb51422
drussell1974 <dave@jazzthecat.co.uk>
2021-12-19 17:41:37 +0000

    minimise MVC scaffold

---
+++ src/bootstrap_template/controllers/placeholder.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# -------------------------------------------------------------------------
+# This is a sample controller
+# this file is released under public domain and you can use without limitations
+# -------------------------------------------------------------------------
+
+# ---- example index page ----
+def index():
+    return dict()
