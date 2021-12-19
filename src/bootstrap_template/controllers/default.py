927d65bf1b79a1046ce8e24440cf2fddf2bbf257
drussell1974 <dave@jazzthecat.co.uk>
2021-12-19 17:41:37 +0000

    minimise MVC scaffold

--- src/bootstrap_template/controllers/default.py
+++ src/bootstrap_template/controllers/default.py
@@ -6,34 +6,8 @@
 
 # ---- example index page ----
 def index():
+    return dict()
 
-    location = "https://via.placeholder.com/"
-    
-    if request.vars.img_w and request.vars.img_h:
-        location = location + request.vars.img_w + "x" + request.vars.img_h
-        
-    return dict(image_placeholder_url=location)
-
-
-# ---- API (example) -----
-@auth.requires_login()
-def api_get_user_email():
-    if not request.env.request_method == 'GET': raise HTTP(403)
-    return response.json({'status':'success', 'email':auth.user.email})
-
-# ---- Smart Grid (example) -----
-@auth.requires_membership('admin') # can only be accessed by members of admin groupd
-def grid():
-    response.view = 'generic.html' # use a generic view
-    tablename = request.args(0)
-    if not tablename in db.tables: raise HTTP(403)
-    grid = SQLFORM.smartgrid(db[tablename], args=[tablename], deletable=False, editable=False)
-    return dict(grid=grid)
-
-# ---- Embedded wiki (example) ----
-def wiki():
-    auth.wikimenu() # add the wiki to the menu
-    return auth.wiki() 
 
 # ---- Action for login/register/etc (required for auth) -----
 def user():
@@ -53,12 +27,3 @@ def user():
     also notice there is http://..../[app]/appadmin/manage/auth to allow administrator to manage users
     """
     return dict(form=auth())
-
-# ---- action to server uploaded static content (required) ---
-@cache.action()
-def download():
-    """
-    allows downloading of uploaded files
-    http://..../[app]/default/download/[filename]
-    """
-    return response.download(request, db)
