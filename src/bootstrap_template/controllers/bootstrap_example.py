8bef45b094fde72a498b4d5dea4ff5067862a777
drussell1974 <dave@jazzthecat.co.uk>
2021-12-20 12:48:55 +0000

    create bootstrap_example controller and views

---
+++ src/bootstrap_template/controllers/bootstrap_example.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+def index(): 
+    main_heading = 'What is Bootstrap?'
+    strapline = 'Bootstrap is a collection of bits of code written in HTML, CSS and JavaScript to quickly build websites'
+    intro = 'Bootstrap is a free front-end framework constisting of a number of features for creating responsive and dynamic web sites.'
+
+    return dict(main_heading=main_heading, strapline_text=strapline, intro_text=intro)
--- src/bootstrap_template/controllers/tutorial.py
+++ src/bootstrap_template/controllers/tutorial.py
@@ -5,8 +5,8 @@
 
 # ---- tutorial index page ----
 def index():
-    main_heading = T('Placeholder content')
-    strapline_text = T('During design and development, dummy content can give the product a look-and-feel.')
-    intro_text = T('Lorem ipsum text is commonly used in the layout in web design to draft content. Likewise, placeholder images provide visual content and indicators for the optimum size images that should be used for the actual content.')
+    main_heading = 'Placeholder content'
+    strapline = 'During design and development, dummy content can give the product a look-and-feel.'
+    intro = 'Lorem ipsum text is commonly used in the layout in web design to draft content. Likewise, placeholder images provide visual content and indicators for the optimum size images that should be used for the actual content.'
 
-    return {'main_heading':main_heading, 'strapline_text':strapline_text, 'intro_text': intro_text}
+    return dict(main_heading=main_heading, strapline_text=strapline, intro_text=intro)
