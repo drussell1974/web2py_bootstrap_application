2a01a767b968029e570ce08c83a0634d798882aa
drussell1974 <dave@jazzthecat.co.uk>
2021-12-20 07:29:58 +0000

    create readable variables

--- src/bootstrap_template/controllers/tutorial.py
+++ src/bootstrap_template/controllers/tutorial.py
@@ -6,7 +6,7 @@
 # ---- tutorial index page ----
 def index():
     main_heading = T('Placeholder content')
-    strapline = T('During design and development, dummy content can give the product a look-and-feel.')
-    content = T('Lorem ipsum text is commonly used in the layout in web design to draft content. Likewise, placeholder images provide visual content and indicators for the optimum size images that should be used for the actual content.')
+    strapline_text = T('During design and development, dummy content can give the product a look-and-feel.')
+    intro_text = T('Lorem ipsum text is commonly used in the layout in web design to draft content. Likewise, placeholder images provide visual content and indicators for the optimum size images that should be used for the actual content.')
 
-    return {'main_heading':main_heading, 'strapline':strapline, 'content': content}
+    return {'main_heading':main_heading, 'strapline_text':strapline_text, 'intro_text': intro_text}
