15070efed2ecd29e609159b01f11488e21e8153b
drussell1974 <dave@jazzthecat.co.uk>
2021-12-20 16:16:14 +0000

    show examples and see more url

--- src/bootstrap_template/controllers/tutorial.py
+++ src/bootstrap_template/controllers/tutorial.py
@@ -5,8 +5,42 @@
 
 # ---- tutorial index page ----
 def index():
-    main_heading = 'Placeholder content'
-    strapline = 'During design and development, dummy content can give the product a look-and-feel.'
-    intro = 'Lorem ipsum text is commonly used in the layout in web design to draft content. Likewise, placeholder images provide visual content and indicators for the optimum size images that should be used for the actual content.'
+    page_text = {
+        'main_heading':'Placeholder content',
+        'strapline_text':'During design and development, dummy content can give the product a look-and-feel.',
+        'intro_text':'Lorem ipsum text is commonly used in the layout in web design to draft content. Likewise, placeholder images provide visual content and indicators for the optimum size images that should be used for the actual content.',
+        'reference_urls': [
+            {'name':'Lorem ipsum','url':'https://loremipsum.io/'}, 
+            {'name':'Placeholder.com','url':'https://placeholder.com/'}
+        ],
+    }
 
-    return dict(main_heading=main_heading, strapline_text=strapline, intro_text=intro)
+    return page_text
+
+
+def bootstrap_example(): 
+    page_text = {   
+        'main_heading': 'What is Bootstrap?',
+        'strapline_text': 'Bootstrap is a collection of bits of code written in HTML, CSS and JavaScript to quickly build websites',
+        'intro_text': 'Bootstrap is a free front-end framework constisting of a number of features for creating responsive and dynamic web sites.',
+        'reference_urls': [
+            {'name':'GetBootstrap 4','url':'https://getbootstrap.com/docs/4.6/getting-started/introduction/'}, 
+        ],
+    }
+
+    return page_text
+
+
+def bootstrap_card(): 
+    page_text = {   
+        'main_heading': 'Card',
+        'strapline_text': 'Bootstrap is a collection of bits of code written in HTML, CSS and JavaScript to quickly build websites',
+        'intro_text': 'Bootstrap is a free front-end framework constisting of a number of features for creating responsive and dynamic web sites.',
+        'reference_urls': [
+            {'name':'GetBootstrap 4','url':'https://getbootstrap.com/docs/4.6/getting-started/introduction/'}, 
+            {'name':'GetBootstrap - Components - Cards','url':'https://getbootstrap.com/docs/4.0/components/card/'}
+        ],
+        'see_more_url':'https://getbootstrap.com/docs/4.0/components/card/'
+    }
+
+    return page_text
