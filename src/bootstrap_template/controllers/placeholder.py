6d612e103d1df5c141a42d13a779ef2512f48961
drussell1974 <dave@jazzthecat.co.uk>
2021-12-20 07:26:02 +0000

    replace create tutorial controller and display placeholder

--- src/bootstrap_template/controllers/placeholder.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# -------------------------------------------------------------------------
-# This is a sample controller
-# this file is released under public domain and you can use without limitations
-# -------------------------------------------------------------------------
-
-# ---- example index page ----
-def index():
-    return dict()
