6f9996fa24f4d2426aeb10ddc324424cb10cf4b6
Dave Russell <drussell@kirkleescollege.ac.uk>
2022-12-30 17:26:47 +0000

    update connection string in example

--- src/bootstrap_template/models/db_custom.example.py
+++ src/bootstrap_template/models/db_custom.example.py
@@ -2,21 +2,35 @@
 # NOTE: the student will need permissions
 db = DAL('sqlite://storage.sqlite')
 
-# -------------------------------------------------------------------------
-# Define your tables below (or better in another model file) for example
-#
-# >>> db.define_table('mytable', Field('myfield', 'string'))
-#
-# Fields can be 'string','text','password','integer','double','boolean'
-#       'date','time','datetime','blob','upload', 'reference TABLENAME'
-# There is an implicit 'id integer autoincrement' field
-# Consult manual for more options, validators, etc.
-#
+production = False
+if production:
+    # -------------------------------------------------------------------------
+    # Set mysql connection
+    #
+    # >>> URI = 'mysql://<username>:<password>@<db_host>/<db_name>' 
+    # 
+    # -------------------------------------------------------------------------
 
-db.define_table('employee_of_the_month',
-    Field('image_path', 'upload'),
-    Field('name', 'string', 30),
-    Field('job_role', 'string', 50),
-    Field('qualities', 'text', 300),
-    Field('quote', 'text', 1000)
-)
+    URI = 'mysql://<username>:<password>@<db_host>/<db_name>' 
+
+    db = DAL(URI, pool_size=20, fake_migrate_all=True)
+
+else:
+    # -------------------------------------------------------------------------
+    # Define your tables below (or better in another model file) for example
+    #
+    # >>> db.define_table('mytable', Field('myfield', 'string'))
+    #
+    # Fields can be 'string','text','password','integer','double','boolean'
+    #       'date','time','datetime','blob','upload', 'reference TABLENAME'
+    # There is an implicit 'id integer autoincrement' field
+    # Consult manual for more options, validators, etc.
+    #
+
+    db.define_table('employee_of_the_month',
+        Field('image_path', 'upload'),
+        Field('name', 'string', 30),
+        Field('job_role', 'string', 50),
+        Field('qualities', 'text', 300),
+        Field('quote', 'text', 1000)
+    )
