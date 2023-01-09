9bc5a4830cef27aecd8bbc2b4e55faf6b270ba8a
Dave Russell <dave@daverussell.co.uk>
2023-01-09 15:56:50 +0000

    modify

--- src/pioneer_cars/private/alter user password.sql
+++ src/pioneer_cars/private/alter user password.sql
@@ -3,4 +3,4 @@ ALTER USER '<username>'@'%'
   PASSWORD EXPIRE INTERVAL 180 DAY
   FAILED_LOGIN_ATTEMPTS 100 PASSWORD_LOCK_TIME 2;
   
-grant all privileges on pioneer_cars.* to '<password>'@'%';
+grant all privileges on pioneer_cars.* to '<username>'@'%';
