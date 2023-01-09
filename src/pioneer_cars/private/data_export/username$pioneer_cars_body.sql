3552eca90760a26b08e6bfe6121f192e787e4d30
Dave Russell <dave@daverussell.co.uk>
2023-01-09 17:44:31 +0000

    update data_export

--- src/pioneer_cars/private/data_export/username$pioneer_cars_body.sql
+++ src/pioneer_cars/private/data_export/username$pioneer_cars_body.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
--- Host: localhost    Database: pioneer_cars
+-- Host: <username>.mysql.eu.pythonanywhere-services.com    Database: <username>$pioneer_cars
 -- ------------------------------------------------------
 -- Server version	5.7.34-log
 
--- src/pioneer_cars/private/data_export/username$pioneer_cars_urgency.sql
+++ src/pioneer_cars/private/data_export/username$pioneer_cars_urgency.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
--- Host: localhost    Database: drussellkc$pioneer_ltd
+-- Host: <username>.mysql.eu.pythonanywhere-services.com    Database: <username>$pioneer_cars
 -- ------------------------------------------------------
 -- Server version	5.7.34-log
 
