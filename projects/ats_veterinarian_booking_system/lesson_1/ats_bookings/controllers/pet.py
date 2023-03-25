deb1ec70421e955de8f04a1202a9e77eec1fce4a
Dave Russell <drussell@kirkleescollege.ac.uk>
2023-03-25 16:24:05 +0000

    [200~Scenario: Client registers a new pet Given the client is at an ATS branch

---
+++ projects/ats_veterinarian_booking_system/lesson_1/ats_bookings/controllers/pet.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# try something like
+def index(): 
+    return dict(message="hello from pet.py")
+
+
+def register():
+    client_id = auth.user_id 
+    form = SQLFORM.factory(
+        Field('pet_name', requires=IS_NOT_EMPTY()), 
+        Field('pet_breed', requires=IS_NOT_EMPTY()), 
+        Field('pet_sex', requires=IS_IN_SET(['M', 'F'])) 
+
+    ) 
+
+    if form.process().accepted: 
+        pet_id = add_pet(form.vars.pet_name, form.vars.pet_breed, form.vars.pet_sex, client_id) 
+        response.flash = f'{form.vars.pet_name} has been registered!' 
+        redirect(URL('default', 'dashboard')) 
+    elif form.errors: 
+        response.error = 'Please correct the errors.' 
+
+    return {'form': form, 'registered_pets': db(db.pets).select() }
