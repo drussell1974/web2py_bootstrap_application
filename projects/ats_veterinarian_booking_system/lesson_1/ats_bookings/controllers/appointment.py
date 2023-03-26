e309ef79f835220886b24401604507d73de71af0
Dave Russell <drussell@kirkleescollege.ac.uk>
2023-03-26 15:55:53 +0100

    set vet_id do not get vet

--- projects/ats_veterinarian_booking_system/lesson_1/ats_bookings/controllers/appointment.py
+++ projects/ats_veterinarian_booking_system/lesson_1/ats_bookings/controllers/appointment.py
@@ -31,36 +31,37 @@ def book():
     return {'form': form, 'appointments': appointments }
 
 
-# function to confirm the appointment 
 def confirm(): 
     
     form = SQLFORM.factory(
         Field('appointment_time', requires=IS_IN_DB(db, 'appointments.id', '%(appointment_time)s')),
+        Field('speciality', requires=IS_IN_SET('x','y','z')),
         table_name='appointments') 
     
     if form.process().accepted: 
+        # get the selected appointment
         appointment_id = form.vars.appointment_time 
         appointment = db(db.appointments.id == appointment_id).select().first() 
+        # get the client for the selected appointment
         client = db(db.client.id == appointment.client_id).select().first() 
         client_id = appointment.client_id 
-        vet = db(db.vets.id == vet_id).select().first() 
         
+        # get vets with speciality from given branch
         vets = db(db.vets.specialty == specialty).select() 
         appointments_query = (db.appointments.branch == branch) & (db.appointments.appointment_time == appointment_time) 
         booked_vets = db(appointments_query).select(db.appointments.vet_id)
 
         for vet in vets: 
             if vet.id not in booked_vets: 
-                vet_id = vet.id
-                break 
+                vet_id = vet.id 
+                break
         
         if vet_id: 
-            return db.appointments.insert(client_id=pet_id, 
-                                           branch=branch, 
-                                           appointment_time=appointment_time, 
-                                           vet_id=vet_id) 
+            db.appointments.insert(client_id=client_id, 
+                                   branch=branch, 
+                                   appointment_time=appointment_time, 
+                                   vet_id=vet_id) 
         
         response.flash = f'Appointment confirmed for {client.client_name} with Dr. {vet.last_name} on {appointment.appointment_time}.' 
-        redirect(URL('default', 'dashboard')) 
 
     return {'form': form}
