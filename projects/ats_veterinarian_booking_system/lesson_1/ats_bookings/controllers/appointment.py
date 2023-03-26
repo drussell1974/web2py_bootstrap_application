baa856bff8e342c14da9a5cd8504cf6a4536bd4c
Dave Russell <drussell@kirkleescollege.ac.uk>
2023-03-26 16:36:08 +0100

    rename appointments table to appointment

--- projects/ats_veterinarian_booking_system/lesson_1/ats_bookings/controllers/appointment.py
+++ projects/ats_veterinarian_booking_system/lesson_1/ats_bookings/controllers/appointment.py
@@ -12,44 +12,44 @@ def book():
         Field('owner_last_name')
     )
     
-    appointments = [] 
+    appointment = [] 
     
     if form.process().accepted: 
         clients = db(db.client.client_name == form.vars.client_name).select()
         
         for client in clients: 
-            appointments_query = (db.appointments.client_id == client.id) 
-            appointments += db(appointments_query).select() 
-        if not appointments: 
+            appointment_query = (db.appointment.client_id == client.id) 
+            appointment += db(appointment_query).select() 
+        if not appointment: 
             response.error = 'No appointments found.' 
         else: 
-            form = SQLFORM.factory(Field('appointment_time', requires=IS_IN_DB(db, 'appointments.id', '%(appointment_time)s')), table_name='appointments') 
+            form = SQLFORM.factory(Field('appointment_time', requires=IS_IN_DB(db, 'appointment.id', '%(appointment_time)s')), table_name='appointment') 
             form.elements('input[type=submit]')[0]['_value'] = 'Confirm Appointment'
     elif form.errors: 
         response.error = 'Please correct the errors.'
 
-    return {'form': form, 'appointments': appointments }
+    return {'form': form, 'appointment': appointment }
 
 
 def confirm(): 
     
     form = SQLFORM.factory(
-        Field('appointment_time', requires=IS_IN_DB(db, 'appointments.id', '%(appointment_time)s')),
+        Field('appointment_time', requires=IS_IN_DB(db, 'appointment.id', '%(appointment_time)s')),
         Field('speciality', requires=IS_IN_SET('x','y','z')),
-        table_name='appointments') 
+        table_name='appointment') 
     
     if form.process().accepted: 
         # get the selected appointment
         appointment_id = form.vars.appointment_time 
-        appointment = db(db.appointments.id == appointment_id).select().first() 
+        appointment = db(db.appointment.id == appointment_id).select().first() 
         # get the client for the selected appointment
         client = db(db.client.id == appointment.client_id).select().first() 
         client_id = appointment.client_id 
         
         # get vets with speciality from given branch
         vets = db(db.vets.specialty == specialty).select() 
-        appointments_query = (db.appointments.branch == branch) & (db.appointments.appointment_time == appointment_time) 
-        booked_vets = db(appointments_query).select(db.appointments.vet_id)
+        appointment_query = (db.appointment.branch == branch) & (db.appointment.appointment_time == appointment_time) 
+        booked_vets = db(appointment_query).select(db.appointment.vet_id)
 
         for vet in vets: 
             if vet.id not in booked_vets: 
@@ -57,7 +57,7 @@ def confirm():
                 break
         
         if vet_id: 
-            db.appointments.insert(client_id=client_id, 
+            db.appointment.insert(client_id=client_id, 
                                    branch=branch, 
                                    appointment_time=appointment_time, 
                                    vet_id=vet_id) 
