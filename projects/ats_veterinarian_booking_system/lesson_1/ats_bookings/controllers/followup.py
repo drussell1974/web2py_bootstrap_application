8c117a067fc2425de7157979e68baf89d777c285
Dave Russell <drussell@kirkleescollege.ac.uk>
2023-03-26 17:13:33 +0100

    rename appointments table to appointment for follow up

--- projects/ats_veterinarian_booking_system/lesson_1/ats_bookings/controllers/followup.py
+++ projects/ats_veterinarian_booking_system/lesson_1/ats_bookings/controllers/followup.py
@@ -5,16 +5,17 @@ def index(): return dict(message="hello from followup.py")
 # function to schedule a follow-up appointment 
 
 def schedule(): 
-
-    pet_id = request.vars.pet_id
+    # schedule from original appointment pass appointment_id
+    
     initial_appointment_id = request.vars.appointment_id 
+
     initial_appointment = db(db.appointment.id == initial_appointment_id).select().first() 
-    vet_id = initial_appointment.vet_id 
+    vet_id = initial_appointment.vet_id
     branch = initial_appointment.branch 
     form = SQLFORM.factory(Field('appointment_time', 'datetime', requires=IS_NOT_EMPTY())) 
     
     if form.process().accepted: 
-        followup_appointment_id = db.appointment.insert(pet_id=pet_id, 
+        followup_appointment_id = db.appointment.insert(client_id=initial_appointment.client_id, 
                                                            branch=branch, 
                                                            appointment_time=form.vars.appointment_time, 
                                                            vet_id=vet_id)
@@ -24,3 +25,4 @@ def schedule():
         response.error = 'Please correct the errors.' 
         
     return {'form': form}
+
