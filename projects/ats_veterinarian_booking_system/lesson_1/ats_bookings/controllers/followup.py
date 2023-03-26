732c83bd2e5d4f98319c16442b0daec376fc7b3a
Dave Russell <drussell@kirkleescollege.ac.uk>
2023-03-26 17:05:53 +0100

    rename appointments table to appointment for follow up

--- projects/ats_veterinarian_booking_system/lesson_1/ats_bookings/controllers/followup.py
+++ projects/ats_veterinarian_booking_system/lesson_1/ats_bookings/controllers/followup.py
@@ -6,17 +6,20 @@ def index(): return dict(message="hello from followup.py")
 
 def schedule(): 
 
-    pet_id = request.vars.pet_id 
+    pet_id = request.vars.pet_id
     initial_appointment_id = request.vars.appointment_id 
-    initial_appointment = db(db.appointments.id == initial_appointment_id).select().first() 
+    initial_appointment = db(db.appointment.id == initial_appointment_id).select().first() 
     vet_id = initial_appointment.vet_id 
     branch = initial_appointment.branch 
     form = SQLFORM.factory(Field('appointment_time', 'datetime', requires=IS_NOT_EMPTY())) 
     
     if form.process().accepted: 
-        followup_appointment_id = add_followup(pet_id, branch, form.vars.appointment_time, vet_id) 
+        followup_appointment_id = db.appointment.insert(pet_id=pet_id, 
+                                                           branch=branch, 
+                                                           appointment_time=form.vars.appointment_time, 
+                                                           vet_id=vet_id)
         response.flash = 'Follow-up appointment scheduled.' 
-        redirect(URL('treatments', 'log', vars={'pet_id': pet_id, 'appointment_id': followup_appointment_id})) 
+        redirect(URL('appointment', 'index', vars={'client_id': client_id, 'appointment_id': followup_appointment_id})) 
     elif form.errors: 
         response.error = 'Please correct the errors.' 
         
